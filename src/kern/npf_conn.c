4bdd436881b9871ddf3d54b18d85475d78722620
kiselev99@gmail.com <kiselev99@gmail.com>
2019-10-19 15:08:17 +0300

    remove some asserts in npf_conn_gc()

--- src/kern/npf_conn.c
+++ src/kern/npf_conn.c
@@ -1181,13 +1181,8 @@ npf_conn_gc(npf_cache_t* npc, npf_t *npf, npf_conndb_t *cd, bool flush,
 		npf_lock_enter(&con->c_lock);
 
 		if ((con->c_flags & CONN_REMOVED) == 0) {
-			npf_conn_t *ret __diagused;
-
-			ret = npf_conndb_remove(cd, fw, key_nwords);
-			KASSERT(ret == con);
-
-			ret = npf_conndb_remove(cd, bk, key_nwords);
-			KASSERT(ret == con);
+			npf_conndb_remove(cd, fw, key_nwords);
+			npf_conndb_remove(cd, bk, key_nwords);
 		}
 
 		/* Flag the removal and expiration. */
